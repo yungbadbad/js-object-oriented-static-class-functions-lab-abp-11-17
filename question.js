a8acf1a414f547abaa2cf39bd458cdad22f59e5b
Sean D <sdoherty8@icloud.com>
2017-11-12 20:53:21 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,16 @@
 'use strict';
+
+class  Question{
+  constructor(content){
+    this.content = content
+  }
+
+save(){
+  this.constructor._All.push(this)
+}
+
+static All(){
+  return this._All;
+}
+  
+}
