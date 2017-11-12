f359eb65cabbf66110ca574b9770b0afd63e4821
Sean D <sdoherty8@icloud.com>
2017-11-12 20:54:05 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -3,14 +3,16 @@
 class  Question{
   constructor(content){
     this.content = content
+    this.save()
   }
 
 save(){
   this.constructor._All.push(this)
 }
 
-static All(){
-  return this._All;
+  static All(){
+    return this._All;
+  }
 }
-  
-}
+
+Question._All = []
